b066666bc8664a3a52e6c905c35646e0c00bba39
Jorden Kreps <jakereps@users.noreply.github.com>
2019-01-25 07:32:26 -0700

    Start on some representative structures and reading FastQs

--- cmd/legion/main.go
+++ cmd/legion/main.go
@@ -22,18 +22,59 @@ func NewFastQ(p string) (*FastQ, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	s := bufio.NewScanner(zr)
 	if err != nil {
 		return nil, err
 	}
+	s.Split(bufio.ScanRunes)
+
 	return &FastQ{
 		scanner: s,
 	}, nil
 }
 
+// Base is a type to hold the enumerated sequence bases
+type Base byte
+
+// Base pairs enumeration
+const (
+	A Base = iota
+	C
+	G
+	T
+)
+
+// Nucleobase holds the pairing of a base and its quality score
+type Nucleobase struct {
+	Base    Base
+	Quality uint8
+}
+
+// Sequence represents a full sequence read
+type Sequence struct {
+	ID   string
+	Data []Nucleobase
+}
+
+func (f *FastQ) Read() (*Sequence, error) {
+	s := &Sequence{}
+	for f.scanner.Scan() {
+		t := f.scanner.Text()
+		if t == "\n" {
+			break
+		}
+		s.ID += t
+	}
+
+	// n := Nucleobase{}
+
+	return s, nil
+}
+
 func main() {
 	sfpath := "/Users/jordenkreps/Downloads/sequences.fastq.gz"
-	bcpath := "/Users/jordenkreps/Downloads/barcodes.fastq.gz"
+	// bcpath := "/Users/jordenkreps/Downloads/barcodes.fastq.gz"
 	// TODO: Read in sequence files (F/R S/P)
 
 	seqs, err := NewFastQ(sfpath)
@@ -43,16 +84,27 @@ func main() {
 	}
 	_ = seqs
 
+	for {
+
+		s, err := seqs.Read()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		_ = s
+		break
+	}
+
 	// TODO: Read in barcodes
 
-	bc, err := NewFastQ(bcpath)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	_ = bc
-	// TODO: Demux/Error correct?
+	// bc, err := NewFastQ(bcpath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return
+	// }
+	// _ = bc
+	// // TODO: Demux/Error correct?
 
-	// TODO: Write results
+	// // TODO: Write results
 
 }
